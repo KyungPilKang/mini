7390faa7c56cc67dad677866ec8316f8db5e7cd4
KyungPilKang <97011445+KyungPilKang@users.noreply.github.com>
2022-03-21 19:36:18 +0900

    update readme

---
+++ Readme.md
@@ -0,0 +1,65 @@
+# Introduction to Studio Lime
+
+## What is Studio Lime?
+- 온라인 교육업 종사자(촬영, 편집자)의 유연한 고용환경 조성
+  - 특정 기업에 의존적인 비대칭적 고용형태 해결 
+- 신입 강사들에대한 높은 수준의 온라인 컨텐츠 제작 환경 제공의 기회
+  - 메이저 강사뿐만 아니라 신입 강사들에게도 고품질 온라인 강의 제작 환경을 제공
+- 궁극적으로 최종 소비자인 학생들에게 합리적인 온라인 강의 컨텐츠 제공에 기여
+  - 온라인 교육시장의 완전경쟁시장 환경을 구축하여 최종 소비자로의 선순환 기대
+<br>
+<br>
+
+# Function Architecture
+
+<details><summary> <b>메인, 회원, 마이페이지, 프리랜서 신청, 관리자</b> </summary>
+
+<img src="images/image1.png" width="100%" height="100%"/>
+
+</details>
+<br>
+<br>
+
+# Class Architecture
+
+<details><summary> <b>메인, 멤버, 프리랜서 신청, 고객문의</b> </summary>
+
+<img src="images/image5.png" width="100%" height="100%"/>
+
+</details>
+<br>
+
+<details><summary> <b>마이페이지, 관리자페이지</b> </summary>
+
+<img src="images/image6.png" width="100%" height="100%"/>
+
+</details>
+<br>
+<br>
+
+# Database Schema
+
+<details><summary> <b>회원</b> </summary>
+
+<img src="images/image2.png" width="100%" height="100%"/>
+
+</details>
+<br>
+
+<details><summary> <b>프리랜서 신청</b> </summary>
+
+<img src="images/image3.png" width="100%" height="100%"/>
+
+</details>
+<br>
+
+<details><summary> <b>고객문의</b> </summary>
+
+<img src="images/image4.png" width="100%" height="100%"/>
+
+</details>
+<br>
+<br>
+
+
+
--- schema/individual_project.sql
+++ schema/individual_project.sql
@@ -1,46 +1,46 @@
 CREATE DATABASE `mini` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 CREATE TABLE `application` (
-  `no` varchar(30) NOT NULL,
-  `id` varchar(30) DEFAULT NULL,
-  `title` varchar(100) DEFAULT NULL,
-  `name` varchar(30) DEFAULT NULL,
-  `email` varchar(30) DEFAULT NULL,
-  `phone` varchar(30) DEFAULT NULL,
+  `no` varchar(1000) NOT NULL,
+  `id` varchar(1000) DEFAULT NULL,
+  `title` varchar(1000) DEFAULT NULL,
+  `name` varchar(1000) DEFAULT NULL,
+  `email` varchar(1000) DEFAULT NULL,
+  `phone` varchar(1000) DEFAULT NULL,
   `etc` varchar(1000) DEFAULT NULL,
-  `type` varchar(30) DEFAULT NULL,
-  `camera_type` varchar(30) DEFAULT NULL,
-  `video_type` varchar(30) DEFAULT NULL,
-  `effect_type` varchar(30) DEFAULT NULL,
-  `doc` varchar(30) DEFAULT NULL,
-  `confirm` varchar(30) DEFAULT NULL,
-  `modal` varchar(30) DEFAULT NULL,
+  `type` varchar(1000) DEFAULT NULL,
+  `camera_type` varchar(1000) DEFAULT NULL,
+  `video_type` varchar(1000) DEFAULT NULL,
+  `effect_type` varchar(1000) DEFAULT NULL,
+  `doc` varchar(1000) DEFAULT NULL,
+  `confirm` varchar(1000) DEFAULT NULL,
+  `modal` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `member` (
-  `id` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `email` varchar(30) DEFAULT NULL,
-  `phone` varchar(30) DEFAULT NULL,
-  `type` varchar(10) DEFAULT NULL,
+  `id` varchar(1000) NOT NULL,
+  `password` varchar(1000) NOT NULL,
+  `name` varchar(1000) NOT NULL,
+  `email` varchar(1000) DEFAULT NULL,
+  `phone` varchar(1000) DEFAULT NULL,
+  `type` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `qna` (
-  `no` varchar(30) NOT NULL,
-  `type` varchar(30) DEFAULT NULL,
-  `id` varchar(30) DEFAULT NULL,
-  `name` varchar(30) DEFAULT NULL,
-  `email` varchar(30) DEFAULT NULL,
-  `phone` varchar(30) DEFAULT NULL,
-  `title` varchar(100) DEFAULT NULL,
+  `no` varchar(1000) NOT NULL,
+  `type` varchar(1000) DEFAULT NULL,
+  `id` varchar(1000) DEFAULT NULL,
+  `name` varchar(1000) DEFAULT NULL,
+  `email` varchar(1000) DEFAULT NULL,
+  `phone` varchar(1000) DEFAULT NULL,
+  `title` varchar(1000) DEFAULT NULL,
   `qtext` varchar(1000) DEFAULT NULL,
   `atext` varchar(1000) DEFAULT NULL,
-  `answerConfirm` varchar(30) DEFAULT NULL,
-  `qmodal` varchar(30) DEFAULT NULL,
-  `amodal` varchar(30) DEFAULT NULL,
+  `answerConfirm` varchar(1000) DEFAULT NULL,
+  `qmodal` varchar(1000) DEFAULT NULL,
+  `amodal` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
